53f4ca207faba1510d4e20be62e3b60ffd20a3e8
rajasri04 <31704754+rajasri04@users.noreply.github.com>
2017-09-08 12:59:02 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -8,7 +8,9 @@ app.use(morgan('combined'));
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
-
+app.get('/tom',function(req,res){
+    res.send('hai i am tom    i am going to search jerry' );
+    });
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
