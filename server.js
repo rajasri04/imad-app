177946fe968a84f8162f20045adf1e9d00a56cce
rajasri04 <31704754+rajasri04@users.noreply.github.com>
2017-09-16 22:55:19 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ app.use(morgan('combined'));
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
-app.get('/', function (req, res) {
+app.get('/ui/tom', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'tom.html'));
 });
 app.get('/ui/style.css', function (req, res) {
