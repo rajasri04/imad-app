61240eb2590ad0c8a8809f0e84848b4d3f3f6f3a
rajasri04 <31704754+rajasri04@users.noreply.github.com>
2017-09-17 15:31:28 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,6 +1,4 @@
-console.log('Loaded!');
-var element= document.getElementById('main');
-element.innerHTML="bye";
+
 var counter= 0;
 var button = document.getElementById('counter');
 button.onclick = function(){
