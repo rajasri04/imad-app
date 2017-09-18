818b5362fddad37650bff12ab7b7b84f6565911f
rajasri04 <31704754+rajasri04@users.noreply.github.com>
2017-09-18 23:47:53 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ app.get('/', function (req, res) {
 
 var pool=new  Pool(config);
 app.get('/score-db',function(req,res){
-    pool.query('select * from score',function(err,result){
+    pool.query('select * from score;',function(err,result){
         if(err)
         {
             res.status(500)send(err.toString());
